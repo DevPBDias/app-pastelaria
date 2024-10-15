ed72fa1ed689e0320ff0d20c7d261f9b1a2623e8
DevPBDias <devpbdias@gmail.com>
2024-10-15 20:08:36 -0300

    feat: created menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,30 @@
+import ChoiceBtn from "@/components/ChoiceBtn";
+import LightLogo from "@/components/LightLogo";
+import OrderBtn from "@/components/OrderBtn";
 import React from "react";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <main>
+      <OrderBtn />
+      <h1>COMECE O SEU PEDIDO, ESCOLHENDO O QUE DESEJA ABAIXO.</h1>
+      <section>
+        <ChoiceBtn
+          name="PASTEL SALGADO"
+          route="/pastel-salgado"
+          sizeText="font-3xl"
+        />
+        <ChoiceBtn
+          name="PASTEL DOCE"
+          route="/pastel-doce"
+          sizeText="font-3xl"
+        />
+        <ChoiceBtn name="CESTA DE PASTÉIS" route="/cesta" sizeText="font-3xl" />
+        <ChoiceBtn name="BEBIDAS" route="/bebidas" sizeText="font-3xl" />
+      </section>
+      <LightLogo />
+    </main>
+  );
 };
 
 export default Menu;
