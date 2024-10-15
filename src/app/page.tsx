c71a38305d58a8158aa9abaf4df94edb634e49ee
DevPBDias <devpbdias@gmail.com>
2024-10-15 19:38:53 -0300

    feat: created hero app page and styled it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/icons/Logo.png";
+
 export default function Home() {
-  return <main className="hero-bg-color w-full h-dvh">Hello</main>;
+  return (
+    <main className="hero-bg-color w-full h-dvh content-center">
+      <section className="content-center gap-12">
+        <Image
+          src={logo}
+          alt="logo for food company"
+          quality={100}
+          priority
+          className="w-[35vw] h-[60vh] object-cover"
+        />
+        <Link className="main-cta" href="/menu">
+          Começar
+        </Link>
+      </section>
+    </main>
+  );
 }
