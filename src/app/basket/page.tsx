e3efbbd947444f53025cddb86fe38167e7711c40
DevPBDias <devpbdias@gmail.com>
2024-10-15 23:24:21 -0300

    feat: creatd drinks and basket page

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -3,32 +3,32 @@ import LightLogo from "@/components/LightLogo";
 import OrderBtn from "@/components/OrderBtn";
 import React from "react";
 
-const Menu = () => {
+const Basket = () => {
   return (
     <main className="px-36 py-28 content-center-col gap-16">
       <OrderBtn />
       <h1 className="font-regular text-6xl text-center w-1/2">
-        ESCOLHA O TAMANHO DO PASTEL QUE DESEJA ABAIXO.
+        ESCOLHA A CESTA DE PASTEL COM A QUANTIDADE DESEJADA.
       </h1>
       <section className="w-1/2 content-center-row flex-wrap gap-32">
         <ChoiceBtn
-          name="3 INGREDIENTES 15CM - R$ 17,00"
-          route="/salt/ingredients"
+          name="CESTA COM 6 PASTÉIS"
+          route="/basket/6-made"
           sizeText="text-2xl"
         />
         <ChoiceBtn
-          name="4 INGREDIENTES 20CM - R$ 21,00"
-          route="/salt/ingredients"
+          name="CESTA COM 8 PASTÉIS"
+          route="/basket/8-made"
           sizeText="text-2xl"
         />
         <ChoiceBtn
-          name="5 INGREDIENTES 25CM - R$ 25,00"
-          route="/salt/ingredients"
+          name="CESTA COM 10 PASTÉIS"
+          route="/basket/10-made"
           sizeText="text-2xl"
         />
         <ChoiceBtn
-          name="PASTEL PRONTO 15CM - R$ 15,00"
-          route="/salt/made"
+          name="CESTA COM 12 PASTÉIS"
+          route="/basket/12-made"
           sizeText="text-2xl"
         />
       </section>
@@ -37,4 +37,4 @@ const Menu = () => {
   );
 };
 
-export default Menu;
+export default Basket;
--- src/app/drinks/page.tsx
+++ src/app/drinks/page.tsx
@@ -3,32 +3,22 @@ import LightLogo from "@/components/LightLogo";
 import OrderBtn from "@/components/OrderBtn";
 import React from "react";
 
-const Menu = () => {
+const Basket = () => {
   return (
     <main className="px-36 py-28 content-center-col gap-16">
       <OrderBtn />
       <h1 className="font-regular text-6xl text-center w-1/2">
-        ESCOLHA O TAMANHO DO PASTEL QUE DESEJA ABAIXO.
+        ESCOLHA A CESTA DE PASTEL COM A QUANTIDADE DESEJADA.
       </h1>
       <section className="w-1/2 content-center-row flex-wrap gap-32">
         <ChoiceBtn
-          name="3 INGREDIENTES 15CM - R$ 17,00"
-          route="/salt/ingredients"
+          name="SUCOS NATURAIS"
+          route="/drinks/organic"
           sizeText="text-2xl"
         />
         <ChoiceBtn
-          name="4 INGREDIENTES 20CM - R$ 21,00"
-          route="/salt/ingredients"
-          sizeText="text-2xl"
-        />
-        <ChoiceBtn
-          name="5 INGREDIENTES 25CM - R$ 25,00"
-          route="/salt/ingredients"
-          sizeText="text-2xl"
-        />
-        <ChoiceBtn
-          name="PASTEL PRONTO 15CM - R$ 15,00"
-          route="/salt/made"
+          name="REFRIGERANTES E CERVEJAS"
+          route="/drinks/refri-beer"
           sizeText="text-2xl"
         />
       </section>
@@ -37,4 +27,4 @@ const Menu = () => {
   );
 };
 
-export default Menu;
+export default Basket;
